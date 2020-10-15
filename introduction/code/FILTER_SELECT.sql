48aea05025c5fdebab8e6f16500ec2405cf802d3
Pedro Tashima <pedrotashima42@gmail.com>
2020-10-15 14:36:12 -0300

    feat: added filter select using dates

--- introduction/code/FILTER_SELECT.sql
+++ introduction/code/FILTER_SELECT.sql
@@ -6,3 +6,8 @@ select * from tbClient where AGE>=22;
 
 # With floats
 select * from tbProducts where LISTED_PRICE between 16.002 AND 16.92;
+
+# With dates
+select * from tbClient where BIRTH_DATE > '1995-01-01';
+# Selecting just the month
+select * from tbClient where month(BIRTH_DATE) = 10;
