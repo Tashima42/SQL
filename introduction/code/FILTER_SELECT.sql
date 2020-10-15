93c9f5f71fc0d28b9b0d9ba16569e67b850569f2
Pedro Tashima <pedrotashima42@gmail.com>
2020-10-15 15:13:41 -0300

    feat: added filter with multiple conditions

--- introduction/code/FILTER_SELECT.sql
+++ introduction/code/FILTER_SELECT.sql
@@ -9,5 +9,12 @@ select * from tbProducts where LISTED_PRICE between 16.002 AND 16.92;
 
 # With dates
 select * from tbClient where BIRTH_DATE > '1995-01-01';
-# Selecting just the month
+# Selecting just the month 
 select * from tbClient where month(BIRTH_DATE) = 10;
+
+# Multiple conditions
+select * from tbProducts where LISTED_PRICE > 10 and LISTED_PRICE < 20 and PACKAGE = 'PET';
+# Multiple or conditions
+select * from tbClient where STATE = 'RJ' or STATE = 'SP';
+# Or condition with multiple filters
+select * from tbClient where (STATE = 'RJ' or STATE = 'SP') or (AGE > 27 and AGE < 40);
