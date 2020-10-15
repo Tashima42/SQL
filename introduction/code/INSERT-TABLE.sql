27f395b31ce228d77530678c48eac8d5ee987acd
Pedro Tashima <pedrotashima42@gmail.com>
2020-10-15 11:06:19 -0300

    feat: added data insert in table

---
+++ introduction/code/INSERT-TABLE.sql
@@ -0,0 +1,13 @@
+INSERT INTO tbProducts (
+	PRODUCT, 
+    NAME,
+    PACKAGE,
+    SIZE,
+    PRICE_LIST
+) VALUES (
+	'1040107',
+    'Light - Watermelon',
+    'Can',
+    '350 ml',
+    4.555
+);
