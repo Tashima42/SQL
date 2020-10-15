4716eefb2eaf60d688e8b4b3db3d62ece4e0a106
Pedro Tashima <pedrotashima42@gmail.com>
2020-10-15 14:20:12 -0300

    feat: added more information to select

--- introduction/code/SELECT.sql
+++ introduction/code/SELECT.sql
@@ -1,2 +1,11 @@
-# Show all the data in a given table
-SELECT * from tbSellers;
+# All fileds from a table
+select * from tbClient;
+
+# Only some fields
+select CPF, NAME from tbClient;
+
+# Limit the amount of regitstries 
+select CPF, NAME from tbClient limit 5;
+
+# Use an aliases
+select CPF as CPF_CUSTOMER from tbClient;
